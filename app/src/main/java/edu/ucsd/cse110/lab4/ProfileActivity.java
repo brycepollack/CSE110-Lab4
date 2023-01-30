51b16dabe80b2d3e3b7329be05e69f8fdbfd54a0
Bryce Pollack <bnpollack@ucsd.edu>
2023-01-30 14:35:51 -0800

    first commit

--- app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
     }
 
-    public void onLaunchProfileClicked(View view) {
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
+    public void onGoBackClicked(View view) {
+        //Intent intent = new Intent(this, MainActivity.class);
+        finish();
+
     }
 }
