bb960e1a68a74b5f68b1788af5e65a3082976606
kcyy127 <66866365+kcyy127@users.noreply.github.com>
2023-01-30 14:50:21 -0800

    Part 2

--- app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
@@ -3,6 +3,7 @@ package edu.ucsd.cse110.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
--- app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
@@ -3,8 +3,10 @@ package edu.ucsd.cse110.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -12,11 +14,38 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        loadProfile();
     }
 
     public void onGoBackClicked(View view) {
-        //Intent intent = new Intent(this, MainActivity.class);
         finish();
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        saveProfile();
+        super.onDestroy();
+    }
+
+    public void loadProfile() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        String name = preferences.getString("name", "default_if_not_found");
+
+        TextView nameField = findViewById(R.id.nameField);
+        if (!name.equals("default_if_not_found")) {
+            nameField.setText(name);
+        }
+    }
+
+    public void saveProfile() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        TextView nameField = findViewById(R.id.nameField);
+        editor.putString("name", nameField.getText().toString());
+
+        editor.apply();
     }
 }
